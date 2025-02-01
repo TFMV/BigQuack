9a629bda606d8fbd47dda51c1715fa43add16a8d
TFMV <mcgeehan@gmail.com>
2025-02-01 11:13:36 -0600

    Add BQ->DuckDB Integration Test

--- bigquery_test.go
+++ bigquery_test.go
@@ -19,7 +19,6 @@ func TestBigQuery(t *testing.T) {
 		t.Fatalf("failed to create BigQuery reader: %v", err)
 	}
 	defer reader.Close()
-	// ignore EOF
 	for {
 		rec, err := reader.Read()
 		if err != nil {
@@ -28,6 +27,48 @@ func TestBigQuery(t *testing.T) {
 			}
 			t.Fatalf("failed to read record: %v", err)
 		}
+		fmt.Println(rec)
+	}
+}
+
+func TestIntegration(t *testing.T) {
+	db, err := bigquack.NewDuckDB()
+	if err != nil {
+		t.Fatalf("failed to create DuckDB: %v", err)
+	}
+	defer db.Close()
+
+	conn, err := db.OpenConnection()
+	if err != nil {
+		t.Fatalf("failed to create DuckDB connection: %v", err)
+	}
+	defer conn.Close()
+
+	// Fetch table data from BigQuery and ingest into DuckDB
+	bq, err := bigquack.NewBigQueryReadClient(context.Background())
+	if err != nil {
+		t.Fatalf("failed to create BigQuery client: %v", err)
+	}
+	reader, err := bq.NewBigQueryReader(context.Background(), "tfmv-371720", "tpch", "nation")
+	if err != nil {
+		t.Fatalf("failed to create BigQuery reader: %v", err)
+	}
+	defer reader.Close()
+
+	rec, err := reader.Read()
+	if err != nil {
+		t.Fatalf("failed to read record: %v", err)
+	}
+	conn.IngestCreateAppend(context.Background(), "nation", rec)
+
+	// Query DuckDB and print results
+	rows, _, _, err := conn.Query(context.Background(), "SELECT * FROM nation")
+	if err != nil {
+		t.Fatalf("failed to query DuckDB: %v", err)
+	}
+
+	for rows.Next() {
+		rec := rows.Record()
 		fmt.Println(rec)
 	}
 }
